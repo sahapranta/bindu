ab10be2b972b3324375f0da8ccf9fe41e4f6e64f
sahapranta <pranta1204@gmail.com>
2020-07-23 18:55:11 +0600

    added commit msg

--- app/Http/Controllers/DeploymentController.php
+++ app/Http/Controllers/DeploymentController.php
@@ -10,12 +10,14 @@ class DeploymentController extends Controller
     {
         $githubPayload = $request->getContent();
         $githubHash = $request->header('X-Hub-Signature');
+        $msg = $request->get('head_commit.message');
         $localToken = env('APP_DEPLOY_SECRET');
         $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
         if (hash_equals($githubHash, $localHash)) {
             Artisan::call('down');
             exec('git fetch');
             exec('git pull');
+            exec("git commit -m $msg");
             Artisan::call('up');
             return TRUE;
         }
